b2a1d72ef023d6fb4bd42fcd2337cf4195eea091
Juan Silva Moreno <juansilvania13@gmail.com>
2026-02-02 20:49:44 +0100

    Creamos la clase clase

---
+++ src/controller/Clase.java
@@ -0,0 +1,27 @@
+package controller;
+
+import java.util.ArrayList;
+
+public class Clase {
+
+    private ArrayList<String> listaAlumnos;
+
+    public Clase() {
+
+        listaAlumnos = new ArrayList<>();
+
+    }
+
+    public void agregar() {
+
+    }
+
+    public void buscar() {
+
+    }
+
+    public void eliminar() {
+
+    }
+
+}
--- src/vista/main.java
+++ src/vista/main.java
@@ -1,13 +1,14 @@
 package vista;
 
 import modelo.Alumno;
+import controller.Clase;
 
 public class main {
 
 public static void main(String[] args) {
     
 Alumno Alumno = new Alumno();
-
+Clase Clase = new Clase();
 }
 
 }
